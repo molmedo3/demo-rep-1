d5fe5b20ec86bb903f632c52bee47d90678b9dab
molmedo3 <molmedo@ucsd.edu>
2022-04-30 19:31:28 -0700

    Update demo1Tester.java

--- demo1Tester.java
+++ demo1Tester.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
+// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar skill-demo-test.java
+// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore skill-demo-test
 
 public class demo1Tester {
     @Test
